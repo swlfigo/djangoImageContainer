a2beccc2581d824947b0cdb5ff2880b4d7c5a931
你见唔到我 <403482009@qq.com>
2018-11-23 14:39:15 +0800

    【Feature】update tips

--- container/views.py
+++ container/views.py
@@ -10,7 +10,7 @@ from .serializers import FileSerializer
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the file index.")
 
 class FileView(APIView):
     parser_classes = (MultiPartParser, FormParser)
